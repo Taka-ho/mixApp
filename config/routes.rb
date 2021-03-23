e8e56665a73b195f4c84eea8740d42bc90bd5349
takanori <j1403239@gmail.com>
2021-03-23 23:08:55 +0900

    修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :likes, only: [:create, :destroy]
-  resources :likes, only: [:create, :destroy]
 
   root 'posts#index'
 end
