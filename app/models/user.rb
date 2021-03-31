4f40b9e99c6a52443c7f967c08caec67753be163
takanori <j1403239@gmail.com>
2021-03-31 18:42:12 +0900

    ユーザー画像のバリデーション完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,24 @@ class User < ApplicationRecord
     validates :password, length: { minimum: 6 }
 
   end
+
+  validate :image_presence, :image_size
+
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
+
+  def image_size
+    if image.blob.byte_size > 1.megabytes
+      image.purge
+      errors.add(:images, "は1つのファイル1MB以内にしてください")
+    end
+  end
+  
 end
