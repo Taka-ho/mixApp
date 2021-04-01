c72800faa2d1a84123dcc214927d0e69768de802
takanori <j1403239@gmail.com>
2021-04-01 16:33:53 +0900

    ユーザー画像修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,11 +4,25 @@ class Movie < ApplicationRecord
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
   belongs_to :user
-    with_options presence: true do
+
+  with_options presence: true do
         validates :title
         validates :introduction
         validates :movie
 
     end
 
+    validate :movie_presence
+
+    def movie_presence
+      if movie.attached?
+        if !movie.content_type.in?(%('movie/mp4 movie/mov'))
+          errors.add(:image, 'にはmp4またはmovファイルを添付してください')
+        end
+      else
+        errors.add(:image, 'ファイルを添付してください')
+      end
+    end
+  
+    
 end
