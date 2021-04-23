f41e3460192a030fa01437e118d289113ee8126a
takanori <j1403239@gmail.com>
2021-04-24 08:54:07 +0900

    docker-compose.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'amazon', port: 3000 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -60,4 +60,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.active_storage.service = :amazon
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
+
 end
