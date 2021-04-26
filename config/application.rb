a20f07329c318a1cd32d74cf8f312515a46989f6
takanori <j1403239@gmail.com>
2021-04-26 14:52:41 +0900

    修正してみました

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module AppName
+  config.assets.initialize_on_precompile = false
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
