0567e2544f89e6890a55c9457259f30298cb3271
takanori <j1403239@gmail.com>
2021-05-12 09:14:09 +0900

    modelの修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,8 @@
 class Blog < ApplicationRecord
     has_rich_text :body
     belongs_to :user
+    has_many_attached :images
+
     has_many :blog_comment,dependent: :destroy
     has_many :blog_likes
     has_many :blog_liked_users, through: :blog_likes, source: :user
