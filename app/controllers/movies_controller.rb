eddeb4b777cee9213ef4bef8673d605cb18a859e
takanori <j1403239@gmail.com>
2021-05-13 10:34:05 +0900

    修正しました

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,7 +18,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = current_user
-    @movie = Movie.new(params.require(:movie).permit(:title,:introduction, :movie))
+    @movie = Movie.create(movie_params)
     if @movie.save
       redirect_to movies_path, notice: '投稿に成功しました'
     else
