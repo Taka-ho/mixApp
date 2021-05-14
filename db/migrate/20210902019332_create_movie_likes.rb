f47009828ae7ad037aa36589a41fd9eabc87cfca
takanori <j1403239@gmail.com>
2021-05-14 18:39:14 +0900

    マイグレーションファイル修正

--- db/migrate/20210902019332_create_movie_likes.rb
+++ db/migrate/20210902019332_create_movie_likes.rb
@@ -1,8 +1,8 @@
 class CreateMovieLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :movie_likes do |t|
-      t.bigint :movie_id, null: false, foreign_key: true
-      t.bigint :user_id, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
